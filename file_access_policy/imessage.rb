e65fc44947a2c15a23e0055c9db83c1b1f9cb794
Craig Bass <craig@madetech.com>
2026-02-16 21:50:23 +0000

    Add IMDPersistenceAgent / imagent

--- file_access_policy/imessage.rb
+++ file_access_policy/imessage.rb
@@ -15,4 +15,6 @@ watch_item "iMessages" do
   process signing_id: "com.apple.imtransferservices.IMTransferAgent", platform_binary: true
   process signing_id: "com.apple.mdworker_shared", platform_binary: true
   process signing_id: "com.apple.Spotlight", platform_binary: true
+  process signing_id: "com.apple.imdpersistence.IMDPersistenceAgent", platform_binary: true
+  process signing_id: "com.apple.imagent", platform_binary: true
 end
